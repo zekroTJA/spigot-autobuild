9712fe09d3abe9fbcee380d3d00c576ea9531349
zekroTJA <riho@live.de>
2022-11-14 13:38:15 +0100

    run pre backup script asynchroneously

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -4,7 +4,7 @@ set -e
 source ./scripts/utils.sh
 
 ./scripts/information.sh
-is_true "$PRE_START_BACKUP" && ./scripts/backup.sh "pre"
+is_true "$PRE_START_BACKUP" && ./scripts/backup.sh "pre" &
 ./scripts/build.sh
 ./scripts/run.sh
 is_true "$POST_START_BACKUP" && ./scripts/backup.sh "post"
